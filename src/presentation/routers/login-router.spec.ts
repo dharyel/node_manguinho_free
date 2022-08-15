deb1573d84e7f9b6044115903b678f37be6a8af7
Dharyel <dshfoxdev@gmail.com>
2022-08-15 15:41:41 -0300

    ensure loginRouter returns 400 if no password is provided

--- src/presentation/routers/login-router.spec.ts
+++ src/presentation/routers/login-router.spec.ts
@@ -8,7 +8,7 @@ interface HttpResponse {
 
 class LoginRouter {
     route (httpRequest: HttpRequest) {
-        if (!httpRequest.body.email) {
+        if (!httpRequest.body.email || !httpRequest.body.password) {
             const httpResponse: HttpResponse = {
                 statusCode: 400
             }
@@ -26,6 +26,22 @@ describe('Login Router', () => {
         const httpRequest: HttpRequest = {
             body: {
                 password: 'any_password'
+            }
+        }
+
+        const httpResponse = sut.route(httpRequest)
+
+        expect(httpResponse?.statusCode).toBe(400)
+    })
+
+    test('Should return 400 if no password is provided', () => {
+        // 400 = bad request
+        // sut = system under test. É quem está sendo testado
+        // nesse caso, sut seria o loginRouter
+        const sut = new LoginRouter()
+        const httpRequest: HttpRequest = {
+            body: {
+                email: 'any_email@mail.com'
             }
         }
 

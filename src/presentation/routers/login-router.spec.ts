023976e6d14c51f58e45f99fe45eb82c01d282df
Dharyel <dshfoxdev@gmail.com>
2022-08-17 23:08:10 -0300

    ensure LoginRouter returns 401 when invalid credentials are provided

--- src/presentation/routers/login-router.spec.ts
+++ src/presentation/routers/login-router.spec.ts
@@ -69,4 +69,17 @@ describe('Login Router', () => {
         sut.route(httpRequest)
         expect(authUseCaseSpy.email).toBe(httpRequest.body.email)
     })
+
+    test('Should return 401 when invalid credentials are provided', () => {
+        const { sut } = makeSut()
+        const httpRequest: IHttpRequest = {
+            body: {
+                email: 'any_email@mail.com',
+                password: 'any_password'
+            }
+        }
+
+        const httpResponse = sut.route(httpRequest)
+        expect(httpResponse?.statusCode).toBe(401)
+    })
 })
--- src/presentation/routers/login-router.ts
+++ src/presentation/routers/login-router.ts
@@ -20,5 +20,9 @@ export class LoginRouter {
         }
 
         this.authUseCase.auth(email, password)
+
+        return {
+            statusCode: 401
+        }
     }
 }

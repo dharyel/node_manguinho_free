632da7f9c69881c905dabe637f17f09bc3c18c70
Dharyel <dshfoxdev@gmail.com>
2022-08-20 08:56:59 -0300

    created emailValidator factory

--- src/utils/email-validator.spec.ts
+++ src/utils/email-validator.spec.ts
@@ -6,16 +6,20 @@ class EmailValidator {
     }
 }
 
+const makeSut = () => {
+    return new EmailValidator()
+}
+
 describe('E-mail validator', () => {
     test('Should return true if validator returns true', () => {
-        const sut = new EmailValidator()
+        const sut = makeSut()
         const isEmailValid = sut.isValid('valid_email@mail.com')
 
         expect(isEmailValid).toBe(true)
     })
 
     test('Should return false if validator returns false', () => {
-        const sut = new EmailValidator()
+        const sut = makeSut()
         const isEmailValid = sut.isValid('invalid_email@')
 
         expect(isEmailValid).toBe(false)
